7e83021c4052e5d033efba4b640e76e75da16b17
Linus Oleander <linus@oleander.nu>
2015-02-10 20:43:01 +0100

    Add ><==

--- part2/TypeChecker.hs
+++ part2/TypeChecker.hs
@@ -51,13 +51,91 @@ inferExp (EDecr exp) =
     (Ok Type_double) -> Ok Type_double
     _      -> Bad "wrong type"
 
+-- a * b
 inferExp (ETimes a b) =
   case inferExp a of
-    (Ok Type_double) -> inferExp b
+    (Ok Type_double) -> Ok Type_double
     (Ok Type_int)    -> inferExp b
     (Ok t)           -> Bad $ "can't run times on " ++ (show t)
     e                -> e
 
+-- a / b
+inferExp (EDiv a b) =
+  case inferExp a of
+    (Ok Type_double) -> Ok Type_double
+    (Ok Type_int)    -> inferExp b
+    (Ok t)           -> Bad $ "can't run div on " ++ (show t)
+    e                -> e
+
+-- a + b
+inferExp (EPlus a b) =
+  case inferExp a of
+    (Ok Type_double) -> 
+      case inferExp b of
+        -- (Ok Type_string) -> Bad "can't run + on double and string"
+        (Ok Type_double) -> Ok Type_double
+        (Ok Type_int)    -> Ok Type_double
+        (Ok t)           -> Bad $ "can't run + on double and " ++ (show t)
+    (Ok Type_int)    -> 
+      case inferExp b of
+        -- (Ok Type_string) -> Bad "can't run + on int and string"
+        (Ok Type_double) -> Ok Type_double
+        (Ok Type_int)    -> Ok Type_int
+        (Ok t)           -> Bad $ "can't run + on int and " ++ (show t)
+    --(Ok Type_string) -> 
+    --  case inferExp b of
+    --    (Ok Type_string) -> Ok Type_string
+    --    (Ok t)           -> Bad $ "can't run + on string and " ++ (show t)
+    (Ok t)           -> Bad $ "can't run plus on " ++ (show t)
+
+-- a - b
+inferExp (EMinus a b) =
+  case inferExp a of
+    (Ok Type_double) -> Ok Type_double
+    (Ok Type_int)    -> inferExp b
+    (Ok t)           -> Bad $ "can't run minus on " ++ (show t)
+    e                -> e
+
+-- a < b
+inferExp (ELt a b) =
+  case inferExp a of
+    (Ok Type_double) -> Ok Type_bool
+    (Ok Type_int)    -> Ok Type_bool
+    (Ok t)           -> Bad $ "can't run < on " ++ (show t)
+    e                -> e
+
+-- a > b
+inferExp (EGt a b) =
+  case inferExp a of
+    (Ok Type_double) -> Ok Type_bool
+    (Ok Type_int)    -> Ok Type_bool
+    (Ok t)           -> Bad $ "can't run < on " ++ (show t)
+    e                -> e
+
+-- a <= b
+inferExp (ELtEq a b) =
+  case inferExp a of
+    (Ok Type_double) -> Ok Type_bool
+    (Ok Type_int)    -> Ok Type_bool
+    (Ok t)           -> Bad $ "can't run < on " ++ (show t)
+    e                -> e
+
+-- a >= b
+inferExp (EGtWq a b) =
+  case inferExp a of
+    (Ok Type_double) -> Ok Type_bool
+    (Ok Type_int)    -> Ok Type_bool
+    (Ok t)           -> Bad $ "can't run < on " ++ (show t)
+    e                -> e
+
+-- a == b
+inferExp (EEq a b) =
+  case inferExp a of
+    (Ok Type_double) -> Ok Type_bool
+    (Ok Type_int)    -> Ok Type_bool
+    -- (Ok Type_string) -> Ok Type_bool
+    (Ok t)           -> Bad $ "can't run == on " ++ (show t)
+    e                -> e
 -- Regular int
 inferExp (EInt i) = Ok Type_int
 
