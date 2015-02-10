b9ba0a9f5826f6da0dbc8536c45c16387ac39076
Linus Oleander <linus@oleander.nu>
2015-02-10 20:14:47 +0100

    Add times

--- part2/TypeChecker.hs
+++ part2/TypeChecker.hs
@@ -24,23 +24,49 @@ inferStm (_:stms)             = inferStm stms
 
 -- n--
 inferExp :: Exp -> Err Type
-inferExp (EDecr exp) =
+inferExp (EPDecr exp) =
   case inferExp exp of
     (Ok Type_int)    -> Ok Type_int
     (Ok Type_double) -> Ok Type_double
     _      -> Bad "wrong type"
 
 -- n++
+inferExp (EPIncr exp) =
+  case inferExp exp of
+    (Ok Type_int)    -> Ok Type_int
+    (Ok Type_double) -> Ok Type_double
+    _      -> Bad "wrong type"
+
+-- ++n
 inferExp (EIncr exp) =
   case inferExp exp of
     (Ok Type_int)    -> Ok Type_int
     (Ok Type_double) -> Ok Type_double
     _      -> Bad "wrong type"
+
+-- --n
+inferExp (EDecr exp) =
+  case inferExp exp of
+    (Ok Type_int)    -> Ok Type_int
+    (Ok Type_double) -> Ok Type_double
+    _      -> Bad "wrong type"
+
+inferExp (ETimes a b) =
+  case inferExp a of
+    (Ok Type_double) -> inferExp b
+    (Ok Type_int)    -> inferExp b
+    (Ok t)           -> Bad $ "can't run times on " ++ (show t)
+    e                -> e
+
 -- Regular int
 inferExp (EInt i) = Ok Type_int
 
 -- Bools
-inferExp ETrue = Ok Type_bool
+inferExp ETrue  = Ok Type_bool
 inferExp EFalse = Ok Type_bool
+
+inferExp (EDouble d) = Ok Type_double
+inferExp (EId id)    = Bad "Can't check type of id"
+
 
 inferExp e = error $ show e ++ " is not implemented yet"
