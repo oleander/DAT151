c33b500e34f5e961532a0eb8f286bf898b38779b
Linus Oleander <linus@oleander.nu>
2015-03-02 19:00:18 +0100

    Better code

--- part1/new/TypeChecker.hs
+++ part1/new/TypeChecker.hs
@@ -52,75 +52,6 @@ checkStm env s = case s of
     _ <- checkStm env stmb
     return env
 
---checkExp :: Exp -> WType -> Env -> Err ()
---checkExp ETrue t env  = checkEqual (WType Type_bool) t
---checkExp EFalse t env = checkEqual (WType Type_bool) t
---checkExp (EInt _) t env = checkEqual (WType Type_int) t
---checkExp (EDouble _) t env = checkEqual (WType Type_double) t
---checkExp (EId i) t env = do
---  t' <- findVarT i env
---  checkEqual (WType t') t
---checkExp (EApp i exps) t env = do
---  returnT <- findFunReturnT i env
---  checkEqual (WType returnT) t
---  argTypes <- findFunArgTs i env
---  if (length exps == length argTypes)
---    then forM_ (zip argTypes exps) $ \(argT, argExp) ->
---    checkExp argExp (WType argT) env
---    else fail "Not enough arguments"
---checkExp (EPDecr e) t env = checkPrePost e env t
---checkExp (EPIncr e) t env = checkPrePost e env t
---checkExp (EIncr e) t env = checkPrePost e env t
---checkExp (EDecr e) t env = checkPrePost e env t
---checkExp (ETimes a b) t env = checkNumBinOp a b t env
---checkExp (EDiv a b) t env = checkNumBinOp a b t env
---checkExp (EPlus a b) t env = checkNumBinOp a b t env
---checkExp (EMinus a b) t env = checkNumBinOp a b t env
----- FIXME these aren't right
---checkExp (ELt a b) t env = checkCompBinOp a b t env
---checkExp (EGt a b) t env = checkCompBinOp a b t env
---checkExp (ELtEq a b) t env = checkCompBinOp a b t env
---checkExp (EGtWq a b) t env = checkCompBinOp a b t env
---checkExp (EEq a b) t env = checkCompBinOp a b t env
---checkExp (ENEq a b) t env = checkCompBinOp a b t env
---checkExp (EAnd a b) t env = checkBoolBinOp a b t env
---checkExp (EOr a b) t env = checkBoolBinOp a b t env
---checkExp (EAss a b) t env = do 
---  checkBinOp a b t env
-----checkExp _ _ _ = fail "not a valid expression"
----- checkEqual _ Type_void _ = return ()
-
---checkPrePost :: Exp -> Env -> WType -> Err ()
---checkPrePost (EId i) env rType = do
---  idT <- findVarT i env
---  if idT `elem` [Type_int, Type_double]
---    then return ()
---    else fail "Can only be applied to int or double"
---  checkEqual (WType idT) rType
---checkPrePost _ _ _ = do
---  fail "Pre/post incr/decr can only be applied to variables"
-
---checkBoolBinOp :: Exp -> Exp -> WType -> Env -> Err ()
---checkBoolBinOp a b rType env = do
---  checkEqual (WType Type_bool) rType
---  checkBinOp a b (WType Type_bool) env
-
---checkCompBinOp :: Exp -> Exp -> WType -> Env -> Err ()
---checkCompBinOp a b t env = do
---  checkEqual (WType Type_bool) t
---  mplus ((checkExp a (WType Type_int) env) >> (checkExp b (WType Type_int) env))
---        ((checkExp a (WType Type_double) env) >> (checkExp b (WType Type_double) env))
-
---checkNumBinOp :: Exp -> Exp -> WType -> Env -> Err ()
---checkNumBinOp a b t env = do
---  mplus ((checkEqual (WType Type_int) t) >> checkBinOp a b (WType Type_int) env)
---        (checkEqual (WType Type_double) t >> checkBinOp a b (WType Type_double) env)
-
---checkBinOp :: Exp -> Exp -> WType -> Env -> Err ()
---checkBinOp a b t env = do
---  checkExp a t env
---  checkExp b t env
-
 -- actual -> expected -> message
 checkEqual :: WType -> WType -> Err ()
 checkEqual actual None = return ()
@@ -206,6 +137,12 @@ checkArgsWithParams (t1:types) (exp:exps) env = do
   t2 <- typeOfExp exp env
   (checkEqual (wrapT t1) (wrapT t2)) >> (checkArgsWithParams types exps env)
 
+typeCheckEq :: Exp -> Exp -> Env -> Err Type
+typeCheckEq a b env = do
+  t1 <- typeOfExp a env
+  t2 <- typeOfExp b env
+  checkEqual (wrapT t1) (wrapT t2) >> return Type_bool
+
 typeOfExp :: Exp -> Env -> Err Type
 typeOfExp ETrue _ = return Type_bool
 typeOfExp EFalse _ = return Type_bool
@@ -228,13 +165,11 @@ typeOfExp (ELt a b) env = typeCheckComp a b env
 typeOfExp (EGt a b) env = typeCheckComp a b env
 typeOfExp (ELtEq a b) env = typeCheckComp a b env
 typeOfExp (EGtWq a b) env = typeCheckComp a b env
-typeOfExp (EEq a b) env = typeCheckComp a b env
-typeOfExp (ENEq a b) env = typeCheckComp a b env
+typeOfExp (EEq a b) env = typeCheckEq a b env
+typeOfExp (ENEq a b) env = typeCheckEq a b env
 typeOfExp (EAnd a b) env = typeCheckBool a b env
 typeOfExp (EOr a b) env = typeCheckBool a b env
 typeOfExp (EAss a b) env = do 
   t1 <- typeOfExp a env
   t2 <- typeOfExp b env
   (checkEqual (wrapT t1) (wrapT t2)) >> return t2
--- typeOfExp (EAss e b) env = fail "e must be a var"
---checkExp _ _ _ = fail "not a valid expression"
